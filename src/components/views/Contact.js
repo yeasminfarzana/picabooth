158650b8a132d970e42d23734cb05327f4779995
yeasminfarzana <yeasmin.oni@gmail.com>
2025-07-24 17:49:53 +0100

    css contact page changed

--- src/components/views/Contact.js
+++ src/components/views/Contact.js
@@ -13,24 +13,27 @@ function Contact() {
             <strong>picabooth.contact@gmail.com</strong>
           </p>
         </div>
-        <div className="group">
-          <form action="https://api.web3forms.com/submit" method="POST">
-            <input
-              type="hidden"
-              name="access_key"
-              value="76d1ce81-675a-4dcd-bd07-3d688ea4f65f"
-            ></input>
-            <label for="name">Your Name</label>
-            <input type="text" id="name" name="name" required></input>
-            <label for="email">Your Email</label>
-            <input type="email" id="email" name="email" required></input>
-            <label for="message_lab">Your Message</label>
-            <textarea id="inputMessage" name="message" required></textarea>
-            <div className="subButton">
-              <input type="submit" value="Submit" id="submitButton"></input>
-            </div>
-          </form>
-        </div>
+
+        <form
+          className="group"
+          action="https://api.web3forms.com/submit"
+          method="POST"
+        >
+          <input
+            type="hidden"
+            name="access_key"
+            value="76d1ce81-675a-4dcd-bd07-3d688ea4f65f"
+          />
+          <label for="name">Your Name</label>
+          <input type="text" id="name" name="name" required />
+          <label for="email">Your Email</label>
+          <input type="email" id="email" name="email" required />
+          <label for="message_lab">Your Message</label>
+          <textarea id="inputMessage" name="message" required></textarea>
+          <div className="subButton">
+            <input type="submit" value="Submit" id="submitButton" />
+          </div>
+        </form>
       </div>
     </div>
   );
